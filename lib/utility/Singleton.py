433779c17bb898bbf9d5ce1c907c8caf337cb66d
JoshuaSgariglia <S1092411@studenti.univpm.it>
2024-03-20 19:26:01 +0100

    Aggiunta implementazione Singleton tramite Metaclass

--- lib/utility/Singleton.py
+++ lib/utility/Singleton.py
@@ -1,7 +1,9 @@
 from functools import wraps
 
+from lib.utility.ObserverClasses import Observable
 
-# Implementazione del design pattern Singleton tramite Decorator
+
+# Implementazione del design pattern Singleton tramite Decorator (__init__ viene eseguito ogni volta)
 def singleton(orig_cls):
     orig_new = orig_cls.__new__
     instance = None
@@ -15,3 +17,22 @@ def singleton(orig_cls):
 
     orig_cls.__new__ = __new__
     return orig_cls
+
+
+# Implementazione del design pattern Singleton tramite Metaclass (__init__ viene eseguito una sola volta)
+class Singleton(type):
+    def __init__(cls, name, bases, mmbs):
+        super(Singleton, cls).__init__(name, bases, mmbs)
+        cls._instance = super(Singleton, cls).__call__()
+
+    def __call__(cls, *args, **kw):
+        return cls._instance
+
+
+class ObservableSingleton(type(Observable)):
+    def __init__(self, name, bases, mmbs):
+        super(ObservableSingleton, self).__init__(name, bases, mmbs)
+        self._instance = super(ObservableSingleton, self).__call__()
+
+    def __call__(self, *args, **kw):
+        return self._instance
