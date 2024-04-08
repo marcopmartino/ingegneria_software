80de117484dbf6efb40926094b4d0efaa5ff9bcd
marcopmartino <s1092106@studenti.univpm.it>
2024-04-08 16:55:25 +0200

    Aggiunta file WorkerList

---
+++ lib/mvc/workerlist/model/WorkerList.py
@@ -0,0 +1,97 @@
+from lib.mvc.workerlist.model.Worker import Worker
+from lib.network.UserNetwork import UserNetwork
+from lib.utility.ObserverClasses import Observable
+from lib.utility.Singleton import singleton, ObservableSingleton
+
+
+class WorkerList(Observable, metaclass=ObservableSingleton):
+
+    def __init__(self):
+        super().__init__()
+        self.__worker_list: list[Worker] = []
+        UserNetwork.stream(self.__stream_handler)
+
+    # Usato internamente per aggiungere un ordine alla lista
+    def __append_worker(self, uid: str, data: any):
+        self.__worker_list.append(Worker(
+            uid, data["name"], data["CF"], data["birth_date"], data["mail"],
+            data["phone"]))
+
+    def __edit_worker(self, path: str, key: str, data: any):
+
+        for index, worker in enumerate(self.__worker_list):
+            if worker.uid == path[1]:
+                match key:
+                    case 'name':
+                        worker.name = data
+                    case 'CF':
+                        worker.CF = data
+                    case 'birth_data':
+                        worker.birth_date = data
+                    case 'phone':
+                        worker.phone = data
+                    case 'mail':
+                        worker.mail = data
+                break
+
+    def __remove_worker(self, path: str):
+        for index, worker in enumerate(self.__worker_list):
+            if worker.uid == path[1]:
+                self.delete(worker.mail)
+                self.__worker_list.remove(worker)
+                break
+
+    # Stream handler che aggiorna automaticamente la lista degli operai
+    def __stream_handler(self, message):
+        print("1")
+        for key in message.keys():
+            print(f"{key}: {message[key]}")
+
+        # Aggiorno la lista degli ordini così che client diversi possano accedere alla stessa versione aggiornata dei
+        # dati (grazie al pattern Singleton)
+        data = message["data"]
+        path = message["path"].split("/")
+        if data is not None:
+            match message["event"]:
+                case "put":
+                    # All'avvio del programma, quando viene caricata l'intera lista di operai
+                    for key, value in data.items():
+                        if type(value) is not dict and key != 'role':
+                            continue
+                        role = value['role'] if type(value) is dict else value
+                        if role in ('worker', 'manager'):
+                            self.__append_worker(key, value)
+                case "patch":
+                    # Quando viene modificato un elemento tramite form apposito
+                    for key, value in data.items():
+                        self.__edit_worker(path, key, value)
+                case "cancel": pass
+        else:
+            # Eliminazione operaio
+            self.__remove_worker(path)
+
+        # Notifico gli osservatori così che possano aggiornarsi (grazie al pattern Observer)
+        message["notifier"] = "WorkerList"
+        self.notify(message)
+
+    # Ritorna la lista  degli operai
+    def get(self) -> list[Worker]:
+        return self.__worker_list
+
+    # Cerca un operaio in base alla mail e lo ritorna
+    def get_by_id(self, mail: str) -> Worker:
+        for worker in self.__worker_list:
+            if worker.mail == mail:
+                return worker
+
+    # Salva un nuovo operaio nel database.
+    @staticmethod
+    def add(worker: Worker) -> str:
+        # Converte l'operaio in dizionario
+        worker_dict = vars(worker)
+        # Salva l'operaio nel database e ne ritorna l'id
+        return UserNetwork.create(worker_dict)
+
+    @staticmethod
+    def delete(email: str):
+        UserNetwork.delete_by_email(email)
---
+++ lib/mvc/workerlist/view/WorkerListView.py
@@ -0,0 +1,85 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import (
+    QTableWidget,
+    QTableWidgetItem, QWidget, QHeaderView, QAbstractItemView, QStyledItemDelegate, QFrame, QPushButton
+)
+
+from lib.mvc.main.view.BaseWidget import BaseWidget
+from lib.mvc.workerlist.controller.WorkerListController import WorkerListController
+from lib.mvc.workerlist.view.AddWorkerWindow import AddWorkerWindow
+from lib.mvc.workerlist.view.EditWorkerWindow import EditWorkerWindow
+from lib.widget.TableWidgets import StandardTable
+from res import Styles
+from res.Dimensions import FontWeight
+
+
+class WorkerListView(BaseWidget):
+    def __init__(self, parent_widget: QWidget = None):
+        super().__init__("worker_list_view", parent_widget)
+        # self.central_frame.setMinimumWidth(800)
+
+        self.setTitleText("Gestione dipendenti")
+        self.setSubtitleText("Clicca due volte su un dipendente per modificarlo")
+
+        self.workerUid = dict()
+        self.isNotConnected = True
+
+        self.headers = ["Nome", "E-mail", "Telefono", "Codice fiscale"]
+
+        self.table = StandardTable(self.central_frame)
+
+        self.table.setObjectName("workers_table")
+        self.table.setHorizontalHeaders(self.headers)
+
+        self.table.setSelectionMode(QAbstractItemView.SingleSelection)
+
+        self.central_layout.addWidget(self.table)
+
+        self.controller = WorkerListController()
+
+        self.font = QFont()
+        self.font.setWeight(FontWeight.BOLD)
+        self.newWorkerButton = QPushButton(self)
+        self.newWorkerButton.setFont(self.font)
+        self.newWorkerButton.setObjectName("new_worker_button")
+        self.newWorkerButton.setText("Aggiungi operaio")
+        self.newWorkerButton.setStyleSheet(Styles.ACCESS_PUSH_BUTTON)
+
+        self.newWorkerButton.clicked.connect(self.open_add_worker)
+
+        self.sidebar_layout.addWidget(self.newWorkerButton, alignment=Qt.AlignTop)
+
+        # Inserisce i dati in tabella e la aggiunge al central_layout
+        def update_table(message: dict[str, any]):
+            worker_list = self.controller.worker_list.get()
+            print("Tabella")
+
+            self.table.setRowCount(len(worker_list))
+            row = 0
+            for worker in worker_list:
+                self.table.setItem(row, 0, QTableWidgetItem(worker.name))
+                self.table.setItem(row, 1, QTableWidgetItem(worker.mail))
+                self.table.setItem(row, 2, QTableWidgetItem(str(worker.phone)))
+                self.table.setItem(row, 3, QTableWidgetItem(str(worker.CF)))
+                self.workerUid[row] = worker.uid
+                row += 1
+            if self.isNotConnected:
+                self.table.cellDoubleClicked.connect(self.open_edit_worker)
+                self.isNotConnected = False
+
+        update_table({})
+        self.controller.worker_list.observe(update_table)
+
+        self.central_layout.addWidget(self.table)
+
+    # Apre la finestra per aggiungere un operaio
+    def open_add_worker(self):
+        add_window = AddWorkerWindow(prevWindow=self)
+        self.setEnabled(False)
+        add_window.show()
+
+    def open_edit_worker(self, selected):
+        edit_window = EditWorkerWindow(prevWindow=self, uid=self.workerUid[selected])
+        self.setEnabled(False)
+        edit_window.show()
